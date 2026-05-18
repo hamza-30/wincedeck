6892a4ebb9b42887d9fa766d241fba68a0990182
Muhammad Hamza <hamzahs2004@gmail.com>
2026-05-18 22:50:32 +0500

    feat: add layout and page components for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../Layout.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import NewProject from "./pages/NewProject.jsx";
+import Project from "./pages/Project.jsx";
+import ProjectSettings from "./pages/ProjectSettings.jsx";
 
-createRoot(document.getElementById('root')).render(
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+        children: [
+          {
+            path: "new",
+            element: <NewProject />,
+          },
+          {
+            path: ":projectId",
+            element: <Project />,
+            children: [
+              {
+                path: "settings",
+                element: <ProjectSettings />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
