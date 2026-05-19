7ebae53965e0d0b0377fc57d00ad502a3eb727af
Muhammad Hamza <hamzahs2004@gmail.com>
2026-05-19 21:42:17 +0500

    feat: implement Navbar component with logo and user authentication links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/wincedeck-logo.png";
+import { FiUser } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  let location = useLocation();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogoClick() {
+    if (user) {
+      navigate("dashboard");
+    } else {
+      navigate("/");
+    }
+  }
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div
+        className={`h-13 mx-auto flex justify-between items-center px-5.5 max-w-7xl`}
+      >
+        <img
+          src={logo}
+          alt="logo"
+          className={`w-fit h-6.75`}
+          onClick={handleLogoClick}
+        />
+
+        {user && (
+          <div className={`flex gap-x-5`}>
+            <div className={`gap-x-1 items-center hidden sm:flex`}>
+              <FiUser className={`text-lg`} />
+              <span className={`text-xs font-medium`}>hamza@windeck.com</span>
+            </div>
+
+            <button
+              className={`flex items-center gap-x-1 border border-gray-300 rounded-sm px-2 py-1 hover:bg-gray-100 active:bg-gray-100`}
+            >
+              <FiLogOut className={`text-sm`} />
+              <span
+                className={`text-[11px] font-semibold hidden sm:inline-block`}
+              >
+                Logout
+              </span>
+            </button>
+          </div>
+        )}
+
+        {location.pathname == "/" && (
+          <div className={`flex gap-x-5 items-center font-semibold`}>
+            <Link
+              to={"login"}
+              className={`text-[11.5px] text-gray-500 hover:text-black active:text-black`}
+            >
+              Login
+            </Link>
+            <Link
+              to={"signup"}
+              className={`text-[11.5px] text-white bg-black px-2.5 py-1 rounded-lg active:scale-95 transition-transform duration-100 ease-in-out text-center`}
+            >
+              Get Started
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className={`border-b border-b-gray-200`}></div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
