b4126677de30305b6b96319ad747e2f83aed21c6
Muhammad Hamza <hamzahs2004@gmail.com>
2026-05-23 02:12:09 +0500

    feat: enhance useProjects hook to include projects collection listener

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as firestoreService from "../services/firestoreService";
 import { useAuthContext } from "../context/AuthContext/AuthContextProvider";
+import { query, collection, where } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export function useProjects() {
   const [loading, setLoading] = useState(false);
+  const [projects, setProjects] = useState([]);
   const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (!user) return;
+
+    let q = query(collection(db, "projects"), where("userId", "==", user.uid));
+    setLoading(true);
+
+    const unsubscribe = firestoreService.subscribeToQuery(q, (snapshot) => {
+      setProjects(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
 
   const createProject = async (projectName) => {
     let customId = `px_${1000 + Math.floor(Math.random() * 2000)}`;
@@ -24,5 +41,5 @@ export function useProjects() {
     }
   };
 
-  return { loading, createProject };
+  return { loading, createProject, projects };
 }
